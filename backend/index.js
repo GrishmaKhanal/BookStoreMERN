0b2d0eeda36b3725ff3704be2a505a17f3d5e1f4
Grishma Raj Khanal <grishmakhanal48@gmail.com>
2023-11-23 10:29:02 +0545

    Add: Vite + TailwindCss

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,23 @@ import mongoose from 'mongoose';
 import { PORT, MONGO_URI } from "./config.js";
 import { Book } from "./models/bookModel.js";
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+// CORS Policy -> Cross-Origin Resource Sharing
+// Middleware for handling CORS Policy
+app.use(cors(
+    {
+        origin: 'http://localhost:5555/',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    }
+));
+
 
 app.get('/', (request, response) => {
     console.log(request);
